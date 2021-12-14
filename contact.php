0d9942fb0c400e228a6212cea076de497df2f86d
t0whid <towhid.hasan004@gmail.com>
2021-12-15 01:42:08 +0600

    Project Uploaded

---
+++ contact.php
@@ -0,0 +1,36 @@
+<?php include("header.php");?>
+<?php include('db_con.php');?>
+<div class="container-fluid">
+    <div class="row" id="wrapper">
+    <?php require_once'menubar.php';?>
+    <div class="col-sm-12">
+        <div class="panel panel-default">
+        <div class="panel-body">
+
+<center><h1>Department of Computer Science and Engineering</h1></center>
+    <hr>
+
+<div class="col-sm-4 col-sm-offset-4" style="height:430px;">
+        <p><b>Contact Information</b><br>
+            <br>
+            Mailing Address:<br>
+            3rd Floor, Science Faculty<br>
+            Islamic University<br>
+            Kushtia 7003, Bangladesh.<br>
+            <br>
+            Chairman<br>
+            Dept. of Computer Science and Engineering<br>
+            Islamic University, Kushtia, Bangladesh<br>
+            <br>
+            Mob : +880 1751639010<br>
+            Tel : +880-71-74901-12 Ext. 2313<br>
+            E-mail : chairman@cse.iu.ac.bd or aktaruzzaman@iu.ac.bd<br>
+            URL: www.cse.iu.ac.bd<br>
+        </p>
+
+</div>
+		</div>
+		</div>
+	</div>
+	</div>
+</div>
